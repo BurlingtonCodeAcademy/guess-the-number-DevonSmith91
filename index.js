c053c8bae0e9a064e48de4e0b098e96b417cf814
DevonSmith91 <DevonSmith91@gmail.com>
2020-02-07 15:00:02 -0500

    added the start of lower/higher functionality

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
+//defining the lower and higher number and creating a variable for the random integer
+let lowNum = 1
+let hiNum = 100
+let randNum = randomInteger(lowNum, hiNum)
+
+
+
 function ask(questionText) {
   return new Promise((resolve, reject) => {
     rl.question(questionText, resolve);
@@ -19,52 +26,54 @@ function capitalize(word) {
   let restOfWord = steralize.slice(1)
   return firstLetter + restOfWord
 }
-
 start();
 
 async function start() {
   //start off with a prompt to play the game.
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
+  console.log("Let's play a game where you make up a number and I try to guess it.")
 
   //allow the user to input a number and have it wait for that to happen
   let secretNumber = await ask("Pick any number between 1 and 100 and let me know what it is.\nI won't peek, I promise...\n");
-  
+
   //return said secret number and say it will now guess.
   console.log('You entered: ' + secretNumber + "\nNow let me guess!");
 
   //Get the computer to guess a random number between 1 and 100
-  console.log(randomInteger(1, 100))
-  
+  console.log(randNum)
+
   //give a prompt about asking if the guess was correct
   let response = await ask("Was I right?! ")
-  
   while (response !== "Y") {
     if (capitalize(response).charAt(0) === "Y") {
-      console.log("I knew it! I'm smarter then you!")
+      console.log("I knew it! I'm smarter then you Human!")
       //kill the process if it guesses correctly
       process.exit();
       //or if it was an incorrect guess 
     } else if (capitalize(response).charAt(0) === "N") {
-      console.log("I NEED TO GUESS AGAIN!" + "\n" + randomInteger(1, 100))
+      //allow the computer to ask if it is higher or lower
+      let highOrLow = await ask("Is your number higher or lower? ")
+        //if higher, guess a higher number
+        if (capitalize(highOrLow).charAt(0) === "H"){
+          //at this point i need to create a consol.log that will spit out a new random number using the current ranNum as the low number
+          console.log(randomInteger(randNum, 100))
+        //if lower, guess a lower number  
+        }else {(capitalize(highOrLow).charAt(0) === "L")
+          //creatue a console.log that will spit out a random number using the old random number as the higher number.
+          console.log(randomInteger(1, randNum))}
       //redefine response to continue loop
       console.log(response = await ask("What about this one? "))
-      
+
       //or if they input something that is not a Yes or No
     } else {
       console.log("That's not an appropriate response.")
       //redefine response to continue loop
       console.log(response = await ask("Please try again.\nWas I right? "))
     }
-    
+
 
   }
-  
-  
-  
-  
+
 }
-
-
 //refrain from nesting if else statements for the entire thing. figure out loops and create loops that will make the
 //function be what I want. It will be easier to update later on in the process.
 
